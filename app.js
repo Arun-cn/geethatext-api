eab26578237814f79cfe9649a332748a4564729d
Arun-cn <arunchandran4321@gmail.com>
2023-01-22 16:52:01 +0400

    add home route

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const app = express();
 
+// response for home route
+app.get('/',(res,rep)=>{
+    rep.send("welcome to our websitenodenode")
+})
+
+
+//listen to port 3000
 app.listen(3000,()=>{
     console.log('server started in port:3000')
 })
